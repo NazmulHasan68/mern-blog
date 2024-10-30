41dca799b3a12bc64c968545a87659629b315ee3
NazmulHasan3580 <n47712338@gmail.com>
2024-10-30 21:14:32 +0600

    mongooDB is connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';  
+dotenv.config();
 
-app.listen(3000, ()=>{
-    console.log('Server is runing on port 3000');
-})
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// route
+app.use('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
