57469c2828bf97c6263602e808804113794d2067
NazmulHasan3580 <n47712338@gmail.com>
2024-10-30 14:30:38 +0600

    create pages abd routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Error from "./pages/Error";
 function App() {
   return (
-    <>
-      <h1 className="">This is Bangladesh</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/*" element={<Error/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,8 @@
+
+export default function Error() {
+  return (
+    <div>
+      This url is not valid
+    </div>
+  )
+}
