3a1ad822b6c9c9b6eed8cdd6142ebd58ee203871
NazmulHasan3580 <n47712338@gmail.com>
2024-11-05 16:38:29 +0600

    Add delete post functionality

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,5 +1,6 @@
-import { Table } from "flowbite-react";
+import { Button, Modal, Table } from "flowbite-react";
 import { useEffect, useState } from "react"
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom";
 
@@ -7,8 +8,9 @@ function DashPost() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPost, setUserPost] =useState([])
   const [showmore, setshowmore] = useState(true)
-  console.log(userPost);
-  
+  const [showModel, setShowModel] = useState(false)
+  const [PostIdToDelete, setPostIdToDelete] = useState('')
+
   const fetechPost = async()=>{
     const res = await fetch(`/api/post/getpost?userId=${currentUser._id}`)
     const data = await res.json()
@@ -42,6 +44,27 @@ function DashPost() {
     }
   }
 
+  const handleDeletePost = async () => {
+    try {
+      const res = await fetch(`/api/post/deletepost/${PostIdToDelete}/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.error(data.message);
+        alert("Error: " + data.message);
+      } else {
+        // Optimistically update the UI
+        setUserPost((prev) => prev.filter((post) => post._id !== PostIdToDelete));
+        setShowModel(false)
+      }
+    } catch (error) {
+      console.error(error.message);
+      alert("An error occurred: " + error.message);
+    }
+  };
+  
+
   return (
     <div className="w-[98%] sm:overflow-hidden overflow-auto scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-400 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-400">
      {currentUser.isAdmin && userPost.length > 0 ? (
@@ -74,7 +97,14 @@ function DashPost() {
                   {post.category}
                 </Table.Cell>
                 <Table.Cell>
-                  <span className=" cursor-pointer text-red-500 hover:font-semibold">Delete</span>
+                  <span 
+                    onClick={()=>{
+                      setShowModel(true)
+                      setPostIdToDelete(post._id)
+                    }} 
+                    className=" cursor-pointer text-red-500 hover:font-semibold">
+                      Delete 
+                  </span>
                 </Table.Cell>
                 <Table.Cell>
                   <Link to={`/updated-post/${post._id}`}>
@@ -98,6 +128,28 @@ function DashPost() {
      ) : (
       <p>You have no Post</p>
      )}
+     {
+      <Modal show={showModel} size="md" onClose={() => setShowModel(false)} popup 
+        position="center" className="flex items-center justify-center h-screen bg-slate-500/55 " >
+      <Modal.Header  />
+      <Modal.Body >
+        <div className="text-center">
+          <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+          <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+            Are you sure you want to delete this post?
+          </h3>
+          <div className="flex justify-center gap-4">
+            <Button color="failure" onClick={() => handleDeletePost(false)}>
+              {"Yes, I'm sure"}
+            </Button>
+            <Button color="gray" onClick={() => setShowModel(false)}>
+              No, cancel
+            </Button>
+          </div>
+        </div>
+      </Modal.Body>
+   </Modal>
+     }
     </div>
   )
 }
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { createController, getPostController } from '../controller/post.controller.js'
+import { createController, deletePostController, getPostController } from '../controller/post.controller.js'
 const PostRouter = express.Router()
 
 PostRouter.post('/create', verifyToken, createController)
-
 PostRouter.get('/getpost', getPostController)
+PostRouter.delete('/deletepost/:postId/:userId', verifyToken, deletePostController)
 
 export default PostRouter
