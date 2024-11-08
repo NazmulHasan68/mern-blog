532fec93a7ed2320e5b34c9f5df225163616dc36
NazmulHasan3580 <n47712338@gmail.com>
2024-11-08 23:32:38 +0600

    fith time try to fix error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   server: {
@@ -8,6 +9,12 @@ export default defineConfig({
         target: 'http://localhost:3000',
         secure: false, // Bypasses SSL verification if the target uses HTTPS
       },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      plugins: [visualizer()],
     },
   },
   plugins: [react()],
